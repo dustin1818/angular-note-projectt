97099adff53fc21fd7136beb4742d23d9483a1cd
codenextdoor <dustinamoda18@gmail.com>
2020-04-23 13:33:59 +0800

    importing note service,router for the note transmission

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-note-details',
@@ -11,14 +13,19 @@ export class NoteDetailsComponent implements OnInit {
 
   note:Note; 
 
-  constructor() { }
+  constructor(private noteService:NotesService,private router:Router) { }
 
   ngOnInit() {
     this.note = new Note();
   }
 
   onSubmit(form:NgForm){
-    console.log(form);
+    this.noteService.add(form.value);
+    this.router.navigateByUrl('/');
+  }
+
+  cancel(){
+    this.router.navigateByUrl('/');
   }
 
 }
