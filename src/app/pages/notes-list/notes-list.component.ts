67473eb16c25bfcd76827c1ab6b8719c8f19052d
codenextdoor <dustinamoda18@gmail.com>
2020-04-23 13:34:42 +0800

    Update notes-list.component.ts

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-notes-list',
@@ -8,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
 export class NotesListComponent implements OnInit {
 
 
+  notes:Note[] = new Array<Note>();
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private notesService: NotesService) { }
+
+  ngOnInit() {
+
+     this.notes = this.notesService.getAll();
   }
 
 }
